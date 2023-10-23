7eeb4c5cfb51d3b3c33c8a8dedba208d2e67c0dd
David Sibley <david.sibley@digital.justice.gov.uk>
2023-10-23 22:49:56 +0100

    created a local to derive a map of lists, where the key is the environment name and the list contains the account IDs

--- terraform/modernisation-platform-account/locals.tf
+++ terraform/modernisation-platform-account/locals.tf
@@ -4,11 +4,19 @@ locals {
   modernisation_platform_ou_id = local.environment_management.modernisation_platform_organisation_unit_id
   pagerduty_integration_keys   = jsondecode(data.aws_secretsmanager_secret_version.pagerduty_integration_keys.secret_string)
 
-  root_users_with_state_access = [ # also includes the organisationsl GHA Role
+  root_users_with_state_access = [ # also includes the organisations GHA Role
     "arn:aws:iam::${local.root_account.master_account_id}:user/ModernisationPlatformOrganisationManagement",
     "arn:aws:iam::${local.root_account.master_account_id}:user/DavidElliott",
     "arn:aws:iam::${local.root_account.master_account_id}:role/ModernisationPlatformGithubActionsRole" # Role with the same permissions as ModernisationPlatformOrganisationManagement for Github OIDC
   ]
+
+  member_account_arns = {
+    for environment in [for file in fileset("../../environments", "*.json") : replace(file, ".json", "")] :
+    environment => [
+      for k, v in local.environment_management.account_ids :
+      v if length(regexall("^${environment}-", k)) > 0
+    ]
+  }
 
   collaborators = jsondecode(file("../../collaborators.json"))
 
