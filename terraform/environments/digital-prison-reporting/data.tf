0c1e0499c15bd88894f41172b109cf59774597e9
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-11-07 12:27:44 +0000

    Move code location

--- terraform/environments/digital-prison-reporting/data.tf
+++ terraform/environments/digital-prison-reporting/data.tf
@@ -6,3 +6,13 @@ data "http" "environments_file" {
 data "tls_certificate" "circleci" {
   url = "https://oidc.circleci.com/org/${data.aws_secretsmanager_secret_version.circleci.secret_string}/.well-known/openid-configuration"
 }
+
+terraform {
+  required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~>4.0"
+    }
+  }
+  required_version = "~> 1.0"
+}
